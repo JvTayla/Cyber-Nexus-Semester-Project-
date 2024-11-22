1115ceebacafb5a8a3215794c993c149a00f7305
MK-Reborn <seeemamakwe@gmail.com>
2024-11-22 13:22:31 +0200

    Mission task combined

---
+++ Assets/MissionTasks.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionTasks : MonoBehaviour
+{
+    private UIScript _UIScript;
+
+    private BigRobotController _BigRobotController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _UIScript = FindAnyObjectByType<UIScript>();
+        _BigRobotController = FindAnyObjectByType<BigRobotController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Robot") && ( _UIScript.NuclearBattery || _BigRobotController.Battery || _BigRobotController.HasNuclearBattery) )
+        {
+            if (_UIScript.tasksdone < 2)
+            {
+                _UIScript.MissionTasks();
+                _UIScript.tasksdone = 2;
+            }
+            
+        }
+        
+        if (other.CompareTag("Robot") && _BigRobotController.Allrecordings)
+        {
+            if (_UIScript.tasksdone < 3)
+            {
+                _UIScript.MissionTasks();
+                _UIScript.tasksdone = 3;
+            }
+           
+        }
+    }
+}
